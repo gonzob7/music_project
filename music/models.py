541027f9607c843a99ca8ed68dac315d04d3917e
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-24 20:18:41 -0700

    added models

--- music/models.py
+++ music/models.py
@@ -4,3 +4,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Musician(models.Model):
+    name = models.CharField(max_length=100)
+    birth_city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    name = models.CharField(max_length=50)
+    date_published = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    date_published = models.DateField()
+    artist = models.ForeignKey(Musician, on_delete=models.CASCADE, null=True)
+    albums = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
