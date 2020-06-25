72fa02103b699f8345ce2457f4b5a3780d5beb02
Gonzalo Birrueta <birruetabusiness@gmail.com>
2020-06-24 20:39:36 -0700

    registered models

--- music/admin.py
+++ music/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Musician)
+admin.site.register(Album)
+admin.site.register(Song)
